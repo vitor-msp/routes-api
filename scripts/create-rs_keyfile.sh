bc394db08cbb072d3d538c3a4fe78e11a4b46923
Vitor Mateus <vitor7jan@gmail.com>
2024-07-16 16:32:51 -0300

    script creating mongo folder

--- scripts/create-rs_keyfile.sh
+++ scripts/create-rs_keyfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-openssl rand -base64 756 > ./mongo/rs_keyfile
+mkdir mongo
+openssl rand -base64 756 > mongo/rs_keyfile
 sudo chmod 400 mongo/rs_keyfile
 sudo chown 999:999 mongo/rs_keyfile
 
