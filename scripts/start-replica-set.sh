14cdd39dac256b984365493c9f555f7bf4d00ac6
Vitor Mateus <vitor7jan@gmail.com>
2024-10-26 10:26:34 -0300

    changed container names

--- scripts/start-replica-set.sh
+++ scripts/start-replica-set.sh
@@ -9,12 +9,12 @@ MONGO_INITDB_ROOT_PASSWORD=`read_env_var MONGO_INITDB_ROOT_PASSWORD`
 MONGO_HOST1_PRIO=`read_env_var MONGO_HOST1_PRIO`
 MONGO_HOST2_PRIO=`read_env_var MONGO_HOST2_PRIO`
 
-docker exec mongo1 mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval '
+docker exec routes-mongo1 mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval '
     rs.initiate({
         _id: "rs0",
         members: [
-            { _id: 0, host: "mongo1:27017", priority: '$MONGO_HOST1_PRIO' },
-            { _id: 1, host: "mongo2:27017", priority: '$MONGO_HOST2_PRIO' }
+            { _id: 0, host: "routes-mongo1:27017", priority: '$MONGO_HOST1_PRIO' },
+            { _id: 1, host: "routes-mongo2:27017", priority: '$MONGO_HOST2_PRIO' }
         ]
     })
 '
